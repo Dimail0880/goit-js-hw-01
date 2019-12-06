a6d907e06c87fafe975806492cdbb99b1aafd12a
dimail0880 <dimail4888@gmail.com>
2019-12-06 18:37:51 +0200

    bugs in task-4 & task-5 fixed

--- js/task-5.js
+++ js/task-5.js
@@ -1,6 +1,6 @@
 let userChoice = prompt(
     "Введите название Вашей страны для оформления доставки выбранного Вами товара"
-).toLowerCase();
+);
 const china = "Китай";
 const chinaPrice = 100;
 const chile = "Чили";
@@ -12,32 +12,34 @@ const indiaPrice = 80;
 const jamaica = "Ямайка";
 const jamaicaPrice = 120;
 
-switch (userChoice) {
-    case "китай":
-        alert(`Доставка в ${china} будет стоить ${chinaPrice} кредитов`);
-        break;
+if (userChoice === null) {
+    alert("Узнайте своё местоположение, и обращайтесь к нам снова!");
+} else {
+    userChoice = userChoice.toLowerCase();
+    switch (userChoice) {
+        case "китай":
+            alert(`Доставка в ${china} будет стоить ${chinaPrice} кредитов`);
+            break;
 
-    case "чили":
-        alert(`Доставка в ${chile} будет стоить ${chilePrice} кредитов`);
-        break;
+        case "чили":
+            alert(`Доставка в ${chile} будет стоить ${chilePrice} кредитов`);
+            break;
 
-    case "австралия":
-        alert(`Доставка в ${australia} будет стоить ${australiaPrice} кредитов`);
-        break;
+        case "австралия":
+            alert(`Доставка в ${australia} будет стоить ${australiaPrice} кредитов`);
+            break;
 
-    case "индия":
-        alert(`Доставка в ${india} будет стоить ${indiaPrice} кредитов`);
-        break;
+        case "индия":
+            alert(`Доставка в ${india} будет стоить ${indiaPrice} кредитов`);
+            break;
 
-    case "ямайка":
-        alert(`Доставка в ${jamaica} будет стоить ${jamaicaPrice} кредитов`);
-        break;
+        case "ямайка":
+            alert(`Доставка в ${jamaica} будет стоить ${jamaicaPrice} кредитов`);
+            break;
 
-    case null:
-        alert("Узнайте своё местоположение, и обращайтесь к нам снова!");
-        break;
 
-    default:
-        alert("В Вашей стране доставка не доступна!");
-        break;
+        default:
+            alert("В Вашей стране доставка не доступна!");
+            break;
+    }
 }
